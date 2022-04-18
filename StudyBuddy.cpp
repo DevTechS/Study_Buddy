f7bd2f261fcbf1f42042d54805de73e252474fab
DevTechS <38230016+DevTechS@users.noreply.github.com>
2022-04-18 00:32:06 -0400

    Add files via upload

--- StudyBuddy.cpp
+++ StudyBuddy.cpp
@@ -14,8 +14,8 @@
 
 using namespace std;
 
-int desktopWidth;
-int desktopHeight;
+int desktopWidth = GetSystemMetrics(SM_CXSCREEN);
+int desktopHeight = GetSystemMetrics(SM_CYSCREEN);
 
 SDL_Event event;
 SDL_Window* window;
@@ -34,6 +34,7 @@ SDL_Texture* body;
 SDL_Texture* pawDown;
 SDL_Texture* pawUp;
 SDL_Texture* tail;
+SDL_Texture* alarmclock;
 
 int HeadSize = 250;
 int BodyW = round(HeadSize * 1100 / 700);
@@ -45,9 +46,11 @@ int mouthW = round(HeadSize * 184.0 / 700);
 int mouthH = round(HeadSize * 178.0 / 700);
 int PawW = round(HeadSize * 202 / 700);
 int PawH = round(HeadSize * 188 / 700);
+int ClockW = 176;
+int ClockH = 216;
 
-double BodyX_V = 1500;
-double BodyY_V = 600;
+double BodyX_V = desktopWidth + 250;
+double BodyY_V = 900;
 double BodyOffsetX_V = 0;
 double BodyOffsetY_V = 0;
 double BodyT_V = 0;
@@ -82,8 +85,12 @@ double Paw3OffsetX_V = 0;
 double Paw3OffsetY_V = 0;
 double Paw3T_V = 0;
 
-double BodyX = 1500;
-double BodyY = 600;
+double ClockX_V = 500;
+double ClockY_V = 500;
+double ClockT_V = 0;
+
+double BodyX = desktopWidth - 200;
+double BodyY = 900;
 double BodyOffsetX = 0;
 double BodyOffsetY = 0;
 double BodyT = 0;
@@ -118,7 +125,12 @@ double Paw3OffsetX = 0;
 double Paw3OffsetY = 0;
 double Paw3T = 0;
 
-POINT mouseP;
+double ClockX = desktopWidth - 750;
+double ClockY = 837;
+double ClockT = 0;
+
+POINT mouse;
+POINT Oldmouse;
 bool mousePressed = 0;
 
 int HeadX = 0;
@@ -133,6 +145,15 @@ int MouthState = 0; // 0-happy, 1-toung, 2-sad
 
 int Tick = 0;
 int AnimationTick = 0;
+int Trick = 0;
+
+int BlinkTimer = 240;
+
+bool fidgeting = 0;
+
+double PetCounter = 0;
+double PetLevel = 0;
+double TrickCounter = 0;
 
 bool MakeWindowTransparent(SDL_Window* window, COLORREF colorKey) {
     SDL_SysWMinfo wmInfo;
@@ -217,16 +238,22 @@ void renderLoop() {
     SDL_Rect eye2Rect = { HeadX + Xoff_r2 - EyeSize / 2, HeadY + Yoff_r2 - EyeSize / 2, EyeSize, EyeSize };
     switch (EyeState) {
     case 0:
-        SDL_RenderCopyEx(renderer, eye, NULL, &eye1Rect, 0, NULL, SDL_FLIP_NONE);
-        SDL_RenderCopyEx(renderer, eye, NULL, &eye2Rect, 0, NULL, SDL_FLIP_NONE);
+        if (BlinkTimer < 10) {
+            SDL_RenderCopyEx(renderer, eyeC, NULL, &eye1Rect, HeadT_V, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(renderer, eyeC, NULL, &eye2Rect, HeadT_V, NULL, SDL_FLIP_NONE);
+        }
+        else {
+            SDL_RenderCopyEx(renderer, eye, NULL, &eye1Rect, 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(renderer, eye, NULL, &eye2Rect, 0, NULL, SDL_FLIP_NONE);
+        }
         break;
     case 1:
-        SDL_RenderCopyEx(renderer, eyeH, NULL, &eye1Rect, HeadT, NULL, SDL_FLIP_NONE);
-        SDL_RenderCopyEx(renderer, eyeH, NULL, &eye2Rect, HeadT, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopyEx(renderer, eyeH, NULL, &eye1Rect, HeadT_V, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopyEx(renderer, eyeH, NULL, &eye2Rect, HeadT_V, NULL, SDL_FLIP_NONE);
         break;
     case 2:
-        SDL_RenderCopyEx(renderer, eyeC, NULL, &eye1Rect, HeadT, NULL, SDL_FLIP_NONE);
-        SDL_RenderCopyEx(renderer, eyeC, NULL, &eye2Rect, HeadT, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopyEx(renderer, eyeC, NULL, &eye1Rect, HeadT_V, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopyEx(renderer, eyeC, NULL, &eye2Rect, HeadT_V, NULL, SDL_FLIP_NONE);
         break;
     }
 
@@ -281,6 +308,9 @@ void renderLoop() {
         break;
     }
 
+    SDL_Rect ClockRect = { ClockX_V, ClockY_V, ClockW, ClockH };
+    SDL_RenderCopyEx(renderer, alarmclock, NULL, &ClockRect, ClockT_V, NULL, SDL_FLIP_NONE);
+
     // Add window transparency (Magenta will be transparent)
     MakeWindowTransparent(window, RGB(255, 0, 255));
     //MakeWindowTransparentAlpha(window2, RGB(255, 0, 255), 100);
@@ -290,11 +320,15 @@ void renderLoop() {
 }
 
 void UpdatePos() {
+    BodyX_V += (BodyX - BodyX_V) * 0.25;
+    BodyY_V += (BodyY - BodyY_V) * 0.25;
+    BodyOffsetX_V += (BodyOffsetX - BodyOffsetX_V)*0.25;
+    BodyOffsetY_V += (BodyOffsetX - BodyOffsetX_V)*0.25;
     BodyT_V += (BodyT - BodyT_V)*0.25;
 
     TailOffsetX_V += (TailOffsetX - TailOffsetX_V)*0.25;
     TailOffsetY_V += (TailOffsetY - TailOffsetY_V)*0.25;
-    TailT_V += (TailT - TailT_V)*0.25;
+    TailT_V += (TailT - TailT_V)*0.50;
 
     HeadOffsetX_V += (HeadOffsetX - HeadOffsetX_V)*0.25;
     HeadOffsetY_V += (HeadOffsetY - HeadOffsetY_V)*0.25;
@@ -318,9 +352,13 @@ void UpdatePos() {
     Paw2OffsetY_V += (Paw2OffsetY - Paw2OffsetY_V)*0.25;
     Paw2T_V += (Paw2T - Paw2T_V)*0.25;
 
-    Paw3OffsetX_V += (Paw3OffsetX - Paw3OffsetX_V)*0.25;
-    Paw3OffsetY_V += (Paw3OffsetY - Paw3OffsetY_V)*0.25;
-    Paw3T_V += (Paw3T - Paw3T_V)*0.25;
+    Paw3OffsetX_V += (Paw3OffsetX - Paw3OffsetX_V) * 0.25;
+    Paw3OffsetY_V += (Paw3OffsetY - Paw3OffsetY_V) * 0.25;
+    Paw3T_V += (Paw3T - Paw3T_V) * 0.25;
+
+    ClockX_V += (ClockX - ClockX_V) * 0.25;
+    ClockY_V += (ClockY - ClockY_V) * 0.25;
+    ClockT_V += (ClockT - ClockT_V) * 0.25;
 }
 
 void Idle() {
@@ -336,8 +374,8 @@ void Idle() {
     HeadOffsetY = 5 * sin(AnimationTick / 80.0);
     HeadT = 5 * sin(AnimationTick / 60.0);
 
-    EyeOffsetX = 10*(double)(mouseP.x - HeadX)/ desktopWidth;
-    EyeOffsetY = 10*(double)(mouseP.y - HeadY)/ desktopHeight;
+    EyeOffsetX = 10 * (double)(mouse.x - HeadX) / desktopWidth;
+    EyeOffsetY = 10 * (double)(mouse.y - HeadY) / desktopHeight;
 
     MouthOffsetX = 0;
     MouthOffsetY = 0;
@@ -366,18 +404,18 @@ void Idle() {
     MouthState = 0;
 }
 
-void Wave() {
+void Petting() {
     BodyOffsetX = 0;
     BodyOffsetY = 0;
-    BodyT = 45*sqrt(abs(sin(AnimationTick/40.0)));
+    BodyT = -5 + sin(AnimationTick / 10.0);
 
     TailOffsetX = 0;
     TailOffsetY = 0;
-    TailT = 30*sin(AnimationTick/4.0);
+    TailT = 15 + 15 * sin(PetCounter / 40.0 + AnimationTick / 40.0);
 
     HeadOffsetX = 0;
-    HeadOffsetY = 0;
-    HeadT = 15*sqrt(abs(sin(AnimationTick / 40.0)));
+    HeadOffsetY = 8 * sin(AnimationTick / 15.0);
+    HeadT = 5 * sin(AnimationTick / 10.0);
 
     EyeOffsetX = 0;
     EyeOffsetY = 0;
@@ -385,13 +423,56 @@ void Wave() {
     MouthOffsetX = 0;
     MouthOffsetY = 0;
 
-    Paw0OffsetX = -25*sqrt(abs(sin(AnimationTick/40.0)));
-    Paw0OffsetY = -150*sqrt(abs(sin(AnimationTick/40.0)));
-    Paw0T = -80*sqrt(abs(sin(AnimationTick/40.0)));
+    Paw0OffsetX = 0;
+    Paw0OffsetY = 2 * sin(PetCounter / 40.0 + AnimationTick / 40.0);
+    Paw0T = 0;
     Paw0State = 0;
 
-    Paw1OffsetX = 150 + 50*sin(AnimationTick/2.0) + 50*sqrt(abs(sin(AnimationTick/40.0)));
-    Paw1OffsetY = -270 + 10*sin(AnimationTick/2.0) - 100*sqrt(abs(sin(AnimationTick/40.0)));
+    Paw1OffsetX = 0;
+    Paw1OffsetY = 2 * cos(PetCounter / 40.0 + AnimationTick / 40.0);
+    Paw1T = 0;
+    Paw1State = 0;
+
+    Paw2OffsetX = 0;
+    Paw2OffsetY = 0;
+    Paw2T = 0;
+    Paw2State = 0;
+
+    Paw3OffsetX = 0;
+    Paw3OffsetY = 0;
+    Paw3T = 0;
+    Paw3State = 0;
+
+    EyeState = 1;
+    MouthState = 1;
+}
+
+void Wave() {
+    BodyOffsetX = 0;
+    BodyOffsetY = 0;
+    BodyT = 45 * sqrt(abs(sin(AnimationTick / 40.0)));
+
+    TailOffsetX = 0;
+    TailOffsetY = 0;
+    TailT = 30 * sin(AnimationTick / 4.0);
+
+    HeadOffsetX = 0;
+    HeadOffsetY = 0;
+    HeadT = 15 * sqrt(abs(sin(AnimationTick / 40.0)));
+
+    EyeOffsetX = 0;
+    EyeOffsetY = 0;
+
+    MouthOffsetX = 0;
+    MouthOffsetY = 0;
+
+    Paw0OffsetX = -25 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0OffsetY = -150 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0T = -80 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0State = 0;
+
+    Paw1OffsetX = 150 + 50 * sin(AnimationTick / 2.0) + 50 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw1OffsetY = -270 + 10 * sin(AnimationTick / 2.0) - 100 * sqrt(abs(sin(AnimationTick / 40.0)));
     Paw1T = 25;
     Paw1State = 1;
 
@@ -409,9 +490,199 @@ void Wave() {
     MouthState = 0;
 }
 
+void Scratch()
+{
+    /*
+    BodyOffsetX = 0;
+    BodyOffsetY = 0;
+    BodyT = -20 + 2 * sin(AnimationTick / 200.0);
+    */
+
+    //BodyOffsetX = 150 + 50 * sin(AnimationTick / 2.0) + 50 * sqrt(abs(sin(AnimationTick / 40.0)));
+    //BodyOffsetY = 270 + 10 * sin(AnimationTick / 2.0) - 100 * sqrt(abs(sin(AnimationTick / 40.0)));
+    BodyT = -10 - sin(AnimationTick / 80.0);
+
+    TailOffsetX = 0;
+    TailOffsetY = 0;
+    TailT = 10 * sin(AnimationTick / 20.0);
+
+    HeadOffsetX = 0;
+    HeadOffsetY = 10 * sin(AnimationTick / 80.0);
+    HeadT = 20;
+
+    EyeOffsetX = 0;
+    EyeOffsetY = 0;
+
+    MouthOffsetX = 0;
+    MouthOffsetY = 0;
+
+    Paw0OffsetX = 0;
+    Paw0OffsetY = 0;
+    Paw0T = 0;
+    Paw0State = 0;
+
+
+    Paw1OffsetX = 0;
+    Paw1OffsetY = 0;
+    Paw1T = 0;
+    Paw1State = 0;
+
+
+    Paw2OffsetX = -200 + 50 * sin(AnimationTick / 4.0) + 50 * sqrt(abs(sin(AnimationTick / 60.0)));
+    Paw2OffsetY = -270;
+    Paw2T = 25;
+    Paw2State = 0;
+
+
+    Paw3OffsetX = 0;
+    Paw3OffsetY = 0;
+    Paw3T = 0;
+    Paw3State = 0;
+
+    EyeState = 1;
+    MouthState = 2;
+}
+
+void Sleepy()
+{
+    /*
+    BodyOffsetX = 0;
+    BodyOffsetY = 0;
+    BodyT = -20 + 2 * sin(AnimationTick / 200.0);
+    */
+
+    BodyOffsetX = 0;//150 + 50 * sin(AnimationTick / 2.0) + 50 * sqrt(abs(sin(AnimationTick / 40.0)));
+    BodyOffsetY = -50;
+    BodyT = 0;
+
+    TailOffsetX = 0;
+    TailOffsetY = 50;
+    TailT = 10 * sin(AnimationTick / 20.0);
+
+    HeadOffsetX = -5 * sin(AnimationTick / 80.0) + 30;
+    HeadOffsetY = 70;//-10 * sin(AnimationTick / 80.0);
+    HeadT = -40;
+
+    EyeOffsetX = 0;
+    EyeOffsetY = 0;
+
+    MouthOffsetX = 0;
+    MouthOffsetY = 0;
+
+    Paw0OffsetX = 0;
+    Paw0OffsetY = 0;
+    Paw0T = 0;
+    Paw0State = 0;
+
+
+    Paw1OffsetX = 0;
+    Paw1OffsetY = 0;
+    Paw1T = 0;
+    Paw1State = 0;
+
+
+    Paw2OffsetX = 0;
+    Paw2OffsetY = 0;
+    Paw2T = 0;
+    Paw2State = 0;
+
+
+    Paw3OffsetX = 0;
+    Paw3OffsetY = 0;
+    Paw3T = 0;
+    Paw3State = 0;
+
+    EyeState = 2;
+    MouthState = 2;
+}
+
+void Playful() {
+    BodyOffsetX = 0;
+    BodyOffsetY = 0;
+    BodyT = -20;
+
+    TailOffsetX = 0;
+    TailOffsetY = 0;
+    TailT = 100 + 15 * sin(AnimationTick / 5.0);
+
+    HeadOffsetX = 0;
+    HeadOffsetY = 5 * sin(AnimationTick / 80.0);
+    HeadT = 5 * sin(AnimationTick / 60.0);
+
+    EyeOffsetX = 10 * (double)(mouse.x - HeadX) / desktopWidth;
+    EyeOffsetY = 10 * (double)(mouse.y - HeadY) / desktopHeight;
+
+    MouthOffsetX = 0;
+    MouthOffsetY = 0;
+
+    Paw0OffsetX = 0;
+    Paw0OffsetY = 0;
+    Paw0T = 0;
+    Paw0State = 0;
+
+    Paw1OffsetX = 0;
+    Paw1OffsetY = 0;
+    Paw1T = 0;
+    Paw1State = 0;
+
+    Paw2OffsetX = 0;
+    Paw2OffsetY = 0;
+    Paw2T = 0;
+    Paw2State = 0;
+
+    Paw3OffsetX = 0;
+    Paw3OffsetY = 0;
+    Paw3T = 0;
+    Paw3State = 0;
+
+    EyeState = 0;
+    MouthState = 1;
+}
+
+void Jump() {
+    BodyOffsetX = 100 * abs(sin(AnimationTick / 40.0));
+    BodyOffsetY = 100 * abs(sin(AnimationTick / 40.0));
+    BodyT = 45 * sqrt(abs(sin(AnimationTick / 40.0)));
+
+    TailOffsetX = -90 * abs(sin(AnimationTick / 40.0)) + 5;
+    TailOffsetY = -90 * sqrt(abs(sin(AnimationTick / 40.0)));
+    TailT = 10 * sin(AnimationTick / 10.0);
+
+    HeadOffsetX = -70 * abs(sin(AnimationTick / 40.0)) + 10;
+    HeadOffsetY = -100 * abs(sin(AnimationTick / 40.0));
+    HeadT = 15 * sqrt(abs(sin(AnimationTick / 40.0)));
+
+    EyeOffsetX = 0;
+    EyeOffsetY = 0;
+
+    MouthOffsetX = 0;
+    MouthOffsetY = 0;
+
+    Paw0OffsetX = -45 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0OffsetY = -350 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0T = -80 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw0State = 0;
+
+    Paw1OffsetX = -25 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw1OffsetY = -300 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw1T = -80 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw1State = 0;
+
+    Paw2OffsetX = -45 * sqrt(abs(sin(AnimationTick / 40.0)));;
+    Paw2OffsetY = -140 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw2T = -60 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw2State = 0;
+
+    Paw3OffsetX = -25 * sqrt(abs(sin(AnimationTick / 40.0)));;
+    Paw3OffsetY = -120 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw3T = -60 * sqrt(abs(sin(AnimationTick / 40.0)));
+    Paw3State = 0;
+
+    EyeState = abs(sin(AnimationTick / 40.0)) > 0.5;
+    MouthState = abs(sin(AnimationTick / 40.0)) > 0.5;
+}
+
 int main(int argc, char* argv[]) {
-    desktopWidth = GetSystemMetrics(SM_CXSCREEN);
-    desktopHeight = GetSystemMetrics(SM_CYSCREEN);
 
     window = SDL_CreateWindow("Study Buddy",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -436,6 +707,7 @@ int main(int argc, char* argv[]) {
     pawDown = IMG_LoadTexture(renderer, "assets/Paw0.png");
     pawUp = IMG_LoadTexture(renderer, "assets/Paw1.png");
     tail = IMG_LoadTexture(renderer, "assets/Tail.png");
+    alarmclock = IMG_LoadTexture(renderer, "assets/alarmclock.png");
 
     /*
     POINT p;
@@ -460,9 +732,72 @@ int main(int argc, char* argv[]) {
 
     bool quit = false;
     while (!quit) {
-        GetCursorPos(&mouseP);
+        Oldmouse = mouse;
+        GetCursorPos(&mouse);
 
-        Idle();
+        if (!--BlinkTimer) {
+            if (rand()%10 < 1) {
+                BlinkTimer = rand() % 120 + 40;
+                fidgeting = 1;
+            }else {
+                BlinkTimer = rand() % 60 + 180;
+                fidgeting = 0;
+            }
+        }
+
+        bool clickingClock = mousePressed && mouse.x > ClockX && mouse.x < ClockX + ClockW && mouse.y > ClockY && mouse.y < ClockY + ClockH;
+
+        if (clickingClock) {
+            cout << "clock" << endl;
+        }
+
+        if (mousePressed && TrickCounter == 0 && !clickingClock) {
+            PetCounter += sqrt(pow(Oldmouse.x - mouse.x, 2) + pow(Oldmouse.y - mouse.y, 2));
+            PetLevel += sqrt(pow(Oldmouse.x - mouse.x, 2) + pow(Oldmouse.y - mouse.y, 2));
+            PetLevel *= 0.99;
+            TrickCounter = 0;
+            Petting();
+        } else {
+            if (PetLevel > 1000) {
+                Playful();
+                if (TrickCounter == 60) {
+                    AnimationTick = 0;
+                    if (mouse.y < desktopHeight / 2) {
+                        Trick = 0;
+                    }
+                    else {
+                        Trick = 1;
+                    }
+                }
+                if (TrickCounter++ > 60) {
+                    switch (Trick) {
+                    case 0:
+                        Jump();
+                        if (AnimationTick == 125) {
+                            PetLevel = 0;
+                            TrickCounter = 0;
+                        }
+                        break;
+                    case 1:
+                        //rollover
+                        if (AnimationTick == 125) {
+                            PetLevel = 0;
+                            TrickCounter = 0;
+                        }
+                        break;
+                    }
+                }
+            } else {
+                PetLevel = 0;
+                PetCounter = 0;
+                if (fidgeting) {
+                    Scratch();
+                }
+                else {
+                    Idle();
+                }
+            }
+        }
 
         UpdatePos();
         renderLoop();
@@ -479,6 +814,12 @@ int main(int argc, char* argv[]) {
             case SDL_MOUSEBUTTONUP:
                 mousePressed = 0;
                 //mousePress(event.button);
+                break;
+            case SDL_WINDOWEVENT_FOCUS_GAINED:
+                mousePressed = 1;
+                break;
+            case SDL_WINDOWEVENT_FOCUS_LOST:
+                mousePressed = 0;
                 break;
             }
         }
